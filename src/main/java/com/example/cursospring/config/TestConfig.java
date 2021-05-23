3a98c5ff2001e4e91432cf79508322394d739b02
Ivan Júnior <ivan.junior2706@gmail.com>
2021-05-23 12:01:35 -0300

    Implementando a entidade Category.

--- src/main/java/com/example/cursospring/config/TestConfig.java
+++ src/main/java/com/example/cursospring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.cursospring.entities.Category;
 import com.example.cursospring.entities.Order;
 import com.example.cursospring.entities.User;
 import com.example.cursospring.entities.enums.OrderStatus;
+import com.example.cursospring.repositories.CategoryRepository;
 import com.example.cursospring.repositories.OrderRepository;
 import com.example.cursospring.repositories.UserRepository;
 
@@ -22,14 +24,26 @@ public class TestConfig implements CommandLineRunner{
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eletronicos");
+		Category cat2 = new Category(null, "Livros");
+		Category cat3 = new Category(null, "Computadores");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
 		Order o1 = new Order(null, Instant.parse("2021-05-22T19:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 		Order o2 = new Order(null, Instant.parse("2021-05-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o3 = new Order(null, Instant.parse("2021-05-21T19:53:07Z"), u2, OrderStatus.CANCELED);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
