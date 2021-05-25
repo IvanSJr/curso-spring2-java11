377a78d202d50eb7e0495b0afde859b9ddfe101d
Ivan Júnior <ivan.junior2706@gmail.com>
2021-05-24 21:36:04 -0300

    Criando exceptions para tratar erro de página não encontrada (404)

--- src/main/java/com/example/cursospring/services/UserService.java
+++ src/main/java/com/example/cursospring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.cursospring.entities.User;
 import com.example.cursospring.repositories.UserRepository;
+import com.example.cursospring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService{
@@ -20,7 +21,7 @@ public class UserService{
 	
 	public User FindById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
